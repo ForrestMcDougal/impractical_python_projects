6fef4dbac5d333e555fea2523d4ca43838fc7c8a
Forrest McDougal <forrestmcdougal@gmail.com>
2019-06-15 22:48:27 -0400

    monty hall

---
+++ monty_hall/birthday_paradox_practice.py
@@ -0,0 +1,26 @@
+"""Calculate probability of a shared birthday per x number of people."""
+import random
+
+max_people = 50
+num_runs = 2_000
+
+print("\nProbability of at least 2 people having the same birthday:\n")
+for people in range(2, max_people + 1):
+    found_shared = 0
+    for run in range(num_runs):
+        bdays = []
+        for i in range(0, people):
+            bday = random.randrange(0, 365)  # ignore leap years
+            bdays.append(bday)
+        set_of_bdays = set(bdays)
+        if len(set_of_bdays) < len(bdays):
+            found_shared += 1
+    prob = found_shared / num_runs
+    print(f"Number people = {people} Prob = {prob:.4f}")
+
+print(
+    """
+According to the Birthday paradox, if there are 23 people in a room, 
+there's a 50 percent chance that 2 of them will share the same birthday.
+"""
+)
